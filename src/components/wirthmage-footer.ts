c5c475e381c342b245929cd831303e146d8a1035
AzamiHaruCat <haruazamicat+git@gmail.com>
2026-02-19 18:25:33 +0900

    about

--- src/components/wirthmage-footer.ts
+++ src/components/wirthmage-footer.ts
@@ -1,8 +1,13 @@
+import { parseSimpleMarkdown } from "@/utils/markdown";
 import { css, type CSSResultGroup, html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, query } from "lit/decorators.js";
 import { repeat } from "lit/directives/repeat.js";
+import { unsafeHTML } from "lit/directives/unsafe-html.js";
+import README_MD from "../../README.md?raw";
 
 const TAG_NAME = "wirthmage-footer";
+
+const readmeContents = unsafeHTML(parseSimpleMarkdown(README_MD));
 
 @customElement(TAG_NAME)
 export class WirthMageFooter extends LitElement {
@@ -12,9 +17,9 @@ export class WirthMageFooter extends LitElement {
       color: #488;
       font-size: 0.85rem;
     }
-    :is(:link, :visited) {
-      color: unset;
-      text-decoration: unset;
+    :is(:link, :visited, button) {
+      all: unset;
+      cursor: pointer;
       &:is(:hover, :active, :focus-visible) {
         text-decoration: underline;
       }
@@ -22,12 +27,64 @@ export class WirthMageFooter extends LitElement {
         outline-color: #8ff;
       }
     }
+    nav {
+      position: absolute;
+      top: 1rlh;
+      right:3dvw;
+    }
     address {
       display: flex;
       gap: 1em;
       font: unset;
     }
+    dialog {
+      box-sizing: border-box;
+      font-size: 0.85rem;
+      padding: 1rem 1rem 3rem;
+      border: 1rem solid #fff;
+      background: #fff;
+      max-width: min(80dvw, 50em);
+      max-height: 80dvh;
+      overflow: hidden;
+      overflow-y: auto;
+      position: relative;
+      &::backdrop {
+        background: #8888;
+        cursor: pointer;
+      }
+      button.close {
+        position: fixed;
+        top: 1rlh;
+        right: 3dvw;
+        color: #fff;
+      }
+      h1, h2 {
+        color: #666;
+        &::before {
+          color: #888;
+          font-size: 1rem;
+          font-weight: normal;
+        }
+      }
+      h1 {
+        font-size: 1.5rem;
+        margin: 1em 0 0;
+        &::before {
+          content: "#" " ";
+        }
+      }
+      h2 {
+        font-size: 1.2rem;
+        margin: 2em 0 0;
+        &::before {
+          content: "##" " ";
+        }
+      }
+    }
   `;
+
+  @query("dialog")
+  private _dialog!: HTMLDialogElement;
 
   protected override render(): unknown {
     const rel = "external noopener noreferrer";
@@ -39,24 +96,47 @@ export class WirthMageFooter extends LitElement {
         rel: `me ${rel}`,
       },
     };
+    const addressLinks = repeat(
+      Object.entries(address),
+      ([key]) => key,
+      ([key, { url, rel }]) =>
+        html`
+          <a href=${url} rel=${rel} target="_blank">[${key}]</a>
+        `,
+    );
     return html`
       <footer>
+        <nav>
+          <button @click=${() => this._dialog.showModal()}>
+            [about this app]
+          </button>
+        </nav>
         <address>
-          &copy; 2026 字見明
-          ${
-      repeat(
-        Object.entries(address),
-        ([key]) => key,
-        ([key, { url, rel }]) =>
-          html`
-              <a href=${url} rel=${rel} target="_blank">[${key}]</a>
-            `,
-      )
-    }
+          &copy; 2026 字見明 ${addressLinks}
         </address>
+        ${this.#renderDialog()}
       </footer>
     `;
   }
+
+  #renderDialog(): unknown {
+    return html`
+      <dialog @click=${this.#handleClickDialog}>
+        <form method="dialog">
+          ${readmeContents}
+          <button class="close" title="close">[x]</button>
+        </form>
+      </dialog>
+    `;
+  }
+
+  #handleClickDialog = (e: MouseEvent): void => {
+    const rect = this._dialog.getBoundingClientRect();
+    const x = e.clientX, y = e.clientY;
+    if (x < rect.left || x > rect.right || y < rect.top || y > rect.bottom) {
+      this._dialog.close();
+    }
+  };
 }
 
 declare global {
---
+++ src/utils/markdown.ts
@@ -0,0 +1,46 @@
+export function parseSimpleMarkdown(text: string): string {
+  return text
+    .replace(/\r\n?/g, "\n")
+    .replace(/&(?!(?:\w+|#(?:[0-9]+|x[0-9a-f]+));)/gi, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/^(#{1,6}) (.*)$/gm, hN)
+    .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>")
+    .replace(/\[(.*?)\]\((.*?)\)/g, a)
+    .replace(/\n\n+/g, "</p><p>")
+    .replace(/\n/g, "<br>")
+    .replace(/^/, "<p>")
+    .replace(/$/, "</p>")
+    .replace(/<br>(<h[1-6]>)/g, "</p>$1")
+    .replace(/(<\/h[1-6]>)<br>/g, "$1<p>")
+    .replace(/<p>\s*<\/p>|<p>(<h[1-6]>)|(<\/h[1-6]>)<\/p>/g, "$1$2")
+    .replace(/(?:<(p|br)>- (?:(?!<\/?p>).)+<\/p>)+/g, ul)
+    .replace(/(?:<(p|br)>\d+\. (?:(?!<\/?p>).)+<\/p>)+/g, ol)
+    .replace(/(<li>(?:(?!<(\/li|p)>).)+)<\/p>/g, "$1");
+}
+
+const externalPattern = new RegExp(
+  `^https?://(?!${location.host.replace(/\./g, "\\.")}(?:[/?#]|$))`,
+  "i",
+);
+const a = (_: string, $1: string, $2: string): string =>
+  externalPattern.test($2)
+    ? `<a href="${$2}" rel="external noopener noreferrer" target="_blank">${$1}</a>`
+    : `<a href="${$2}">${$1}</a>`;
+
+const hN = (_: string, $1: string, $2: string): string =>
+  `<h${$1.length}>${$2.trim()}</h${$1.length}>`;
+
+const ul = ($0: string): string =>
+  `<ul>${
+    $0
+      .replace(/<br>- /g, "</p><p>- ")
+      .replace(/<p>- (.+?)<\/p>/g, "<li>$1</li>")
+  }</ul>`;
+
+const ol = ($0: string): string =>
+  `<ol>${
+    $0
+      .replace(/<br>\d+\. /g, "</p><p>1. ")
+      .replace(/<p>\d+\. (.+?)<\/p>/g, "<li>$1</li>")
+  }</ol>`;
